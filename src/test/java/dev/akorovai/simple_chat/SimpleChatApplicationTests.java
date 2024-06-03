9c27dfe5c86842d581c362103bd7c138d448c4ca
akorovai <aktey.main@gmail.com>
2024-06-03 16:55:04 +0200

    feat: project initialization, creation docker-compose.yml

---
+++ src/test/java/dev/akorovai/simple_chat/SimpleChatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.akorovai.simple_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
